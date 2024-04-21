9673c52426a2ae10da5d8e719dd0f47f3ed34dd7
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2024-04-21 03:45:38 +0200

    [#57999] arm: cp14: Add `DBGDSCRint` register
    Zephyr's cmsis_core_a_r_ext.h: __get_DBGDSCR issues this:
    __get_CP(14, 0, result, 0, 1, 0);

--- arch/arm/system_registers.c
+++ arch/arm/system_registers.c
@@ -604,6 +604,8 @@ static ARMCPRegInfo feature_v7_registers[] = {
     /* DBGDRAR and DBGDSAR: v7 only. Always RAZ since we
      * don't implement memory mapped debug components
      */
+
+    ARM32_CP_REG_DEFINE(DBGDSCRint,       14,   0,   0,   1,   0,   0,  RO | CONST(0)) // Debug Status and Control internal
     ARM32_CP_REG_DEFINE(DBGDSCRext,       14,   0,   0,   2,   2,   0,  RO | CONST(0)) // Debug Status and Control external
     ARM32_CP_REG_DEFINE(DBGDRAR,          14,   0,   1,   0,   0,   0,  RO, FIELD(cp14.c1_dbgdrar)) // Debug ROM Address Register
     ARM32_CP_REG_DEFINE(DBGDSAR,          14,   0,   2,   0,   0,   0,  RO, FIELD(cp14.c2_dbgdsar)) // Debug Self Address Offset Register
