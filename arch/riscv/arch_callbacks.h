fce402ca4422ff5374c3bc021861e73473ab2813
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-06-18 17:14:50 +0200

    [#59539] riscv: Add default tlib_clear_edge_interrupt callback

--- arch/riscv/arch_callbacks.h
+++ arch/riscv/arch_callbacks.h
@@ -11,3 +11,4 @@ void tlib_mip_changed(uint64_t value);
 int32_t tlib_handle_custom_instruction(uint64_t id, uint64_t opcode);
 void tlib_handle_post_opcode_execution_hook(uint32_t id, uint64_t pc);
 void tlib_handle_post_gpr_access_hook(uint32_t register_index, uint32_t isWrite);
+void tlib_clic_clear_edge_interrupt(void);
